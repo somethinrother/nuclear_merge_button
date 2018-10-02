5eaf54b95c5189c8b7763d37c3c75ed08a11144e
James <jamesneilson4@gmail.com>
2018-10-01 22:19:39 -0400

    Reformat

--- github_brain.rb
+++ github_brain.rb
@@ -1,39 +1,39 @@
 require 'github_api'
 
 class GithubBrain
-	attr_accessor :user, :repo
+  attr_accessor :user, :repo
 	
-	def initialize(user, repo)
-		@user = user
-		@repo = repo
-		configure
+  def initialize(user, repo)
+	@user = user
+	@repo = repo
+	configure
+  end
+	
+  def get_branch_number
+	
+  end
+	
+  def merge_branch
+	list = github.pull_requests.list
+	branch_name = list.body[0].head.ref
+	github.pull_requests.merge number: '3'
+  end
+	
+  def delete_branch
+	github.git_data.references.delete @user, @repo, 'heads/new_branch'
+  end
+	
+  private
+	
+  def configure
+	Github.configure do |c|
+	  c.oauth_token = ENV['GITHUB_ACCESS_TOKEN']
+	  c.repo = @repo
+	  c.user = @user
 	end
+  end
 	
-	def get_branch_number
-	
-	end
-	
-	def merge_branch
-		list = github.pull_requests.list
-		branch_name = list.body[0].head.ref
-		github.pull_requests.merge number: '3'
-	end
-	
-	def delete_branch
-		github.git_data.references.delete @user, @repo, 'heads/new_branch'
-	end
-	
-	private
-	
-	def configure
-		Github.configure do |c|
-			c.oauth_token = ENV['GITHUB_ACCESS_TOKEN']
-			c.repo = @repo
-			c.user = @user
-		end
-	end
-	
-	def github
-		Github.new
-	end
+  def github
+	Github.new
+  end
 end
