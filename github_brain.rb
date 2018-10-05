08b57ed0e6dd7ad3c31391c7b594d3b2b3bc75b9
James Neilson <james@quandl.com>
2018-10-04 20:09:12 -0400

    Reformat app to work with gemfile

--- github_brain.rb
+++ github_brain.rb
@@ -1,31 +1,35 @@
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'bundler/setup'
 require 'github_api'
 
+# Class to control interaction with the Github API
 class GithubController
   attr_accessor :user, :repo
-	
+
   def initialize(user, repo)
     @user = user
     @repo = repo
     configure
   end
-	
-  def get_all_branches
-	Github::Client::Repos.new.branches.list
+
+  def retrieve_all_branches
+    Github::Client::Repos.new.branches.list
   end
-  
+
   def pull_requests
-	github.pull_requests.list
+    github.pull_requests.list
   end
-	
+
   def merge_branch
-    branch_name = list.body[0].head.ref
-    github.pull_requests.merge number: '3'
+    github.pull_requests.merge number: '6'
   end
-	
+
   def delete_branch
     github.git_data.references.delete @user, @repo, 'heads/new_branch'
   end
-	
+
   def configure
     Github.configure do |c|
       c.oauth_token = ENV['GITHUB_ACCESS_TOKEN']
@@ -33,7 +37,7 @@ class GithubController
       c.user = @user
     end
   end
-	
+
   def github
     Github.new
   end
